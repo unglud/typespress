26e2d334eff100d52305d3d179d182c2ae6287b5
Aleksandr Matrosov <unglud@gmail.com>
2021-04-19 17:51:34 +0200

    parsing from bodies

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from "express";
 
 const router = Router();
-router.get("/", (req: Request, res: Response) => {
+router.get("/login", (req: Request, res: Response) => {
   res.send(`
-  <div>
-    <h1>Hi There</h1>
-</div>
+  <form method="post">
+  <div><label for="">Email</label> <input type="text" name="email"/></div>
+  <div><label for="">Password</label> <input type="password" name="password"/></div>
+  <button>Submit</button>
+</form>
   `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email + password);
 });
 
 export { router };
