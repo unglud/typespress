810dd7be79014ce739693e545f371ad168c79805
Aleksandr Matrosov <unglud@gmail.com>
2021-04-21 09:57:50 +0200

    checking login status

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -24,10 +24,20 @@ router.post("/login", (req: RequestWithBody, res: Response) => {
     email === "hi@higher.com" &&
     password === "password"
   ) {
-    req.session = {loggedIn:true};
-    res.redirect('/');
+    req.session = { loggedIn: true };
+    res.redirect("/");
   } else {
     res.send("Invalid email or password");
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div><div>You are logged in</div><a href="/logout">Logout</a></div>`);
+  } else {
+    res.send(`
+    <div><div>You are not logged in</div><a href="/login">Login</a></div>`);
   }
 });
 
