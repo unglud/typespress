3043c83679eb2a379b39dfaecbade0a3cf4c1d31
Aleksandr Matrosov <unglud@gmail.com>
2021-04-22 13:20:44 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import { router } from "./routes/loginRoutes";
-import {router as controllerRouter} from "./controllers/decorators/controller"
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
 import './controllers/LoginController'
+import { AppRouter } from './appRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["asdf"] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
