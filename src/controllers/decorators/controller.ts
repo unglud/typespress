b76885a96044908d9c60267bee95c06489042878
Aleksandr Matrosov <unglud@gmail.com>
2021-04-22 14:05:15 +0200

    impliment validator

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -5,7 +5,21 @@ import { MetadataKeys } from "./MetadataKeys";
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
 function bodyValidators(keys: string): RequestHandler {
-  return function (req: Request, res: Response, next: NextFunction) {};
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send("Invalid request");
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send("Invalid request");
+        return;
+      }
+    }
+
+    next();
+  };
 }
 
 export function controller(routePrefix: string) {
@@ -30,8 +44,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
